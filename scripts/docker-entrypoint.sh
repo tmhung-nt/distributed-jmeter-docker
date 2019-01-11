1013ef8f40017aeee2d0412476c22fefc88b6993
HungTran-PC <tranmanhhung@tma.com.vn>
2019-01-12 06:36:47 +0700

    modify to run distributed testing with jmeter 4.0

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 set -e
 
+timestamp=$(date +%Y%m%d_%H%M%S)
+jmeter_path=/mnt/jmeter
+
 case $1 in
     master)
         tail -f /dev/null
         ;;
     server)
-        $JMETER_HOME/bin/jmeter-server \
-            -Dserver.rmi.localport=50000 \
-            -Dserver_port=1099
+        $JMETER_HOME/bin/jmeter \
+            -n -s \
+            -Jclient.rmi.localport=7000 -Jserver.rmi.localport=60000
+            -Jserver.rmi.ssl.disable=true \
+            -j ${jmeter_path}/server/slave_${timestamp}_${IP_ADD:9:3}.log 
         ;;
     *)
         echo "Sorry, this option doesn't exist!"
